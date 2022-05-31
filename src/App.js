fee22b9c33c70bc35243572aa11a5072f70cd7cc
hansohh <106541045+hansohh@users.noreply.github.com>
2022-05-31 12:14:20 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import { IconName } from "react-icons/fa";
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="green-nav">
+        Line
+      </div>
+
+
+        <div className="menu-bg">
+        <h4>CHATS</h4>
+        <h4>STATUS</h4>
+        <h4>PEOPLE</h4>
+        </div>
+
+
+      <div className="Search">
+      <input type="text"/>
+      <button>Search</button>
+      </div>
+
+
+      <div className="Add">
+      <h4>New Group</h4>
+      </div>
+      <div className="Add">
+      <h4>New phone number</h4>
+      </div>
+
+      <div className="Initial">
+        <h4>A</h4>
+        </div>
+
+        <div className="List">
+
+        <h4>A</h4>
+ 
+
+      <div className="Message">
+        <h4> Hi! </h4>
+      </div>
+      </div>
+
+    </div>
+
+
+  );
+}
+
+export default App;
